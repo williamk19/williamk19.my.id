4a287b5e3908a69f247f8d2f361867b4406c0ac7
williamk19 <williamkurniawan1144@gmail.com>
2023-04-02 21:41:55 +0700

    project init

---
+++ next-seo.config.js
@@ -0,0 +1,18 @@
+const defaultSEOConfig = {
+  title: "williamk19",
+  titleTemplate: "%s | williamk19",
+  defaultTitle: "williamk19",
+  description: "Frontend Web Engineer",
+  canonical: "https://williamk19.com",
+  openGraph: {
+    url: "https://williamk19.com",
+    title: "williamk19",
+    description: "Frontend Web Engineer",
+    siteName: "williamk19 personal site",
+  },
+  additionalLinkTags: [
+    { rel: 'shortcut icon', href: '/icon/favicon.ico' },
+  ]
+};
+
+export default defaultSEOConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
+import Head from 'next/head';
+import { DefaultSeo } from 'next-seo';
+import defaultSEOConfig from '../../next-seo.config';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+      </Head>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'dark',
+        }}>
+        <DefaultSeo
+          {...defaultSEOConfig}
+        />
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
