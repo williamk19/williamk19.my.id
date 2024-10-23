5956dd754fddd8a82ef5076e2c05434ca7609964
williamk19 <williamkurniawan1144@gmail.com>
2024-10-23 20:59:46 +0700

    remove pb authentication

--- src/lib/pocketbase/index.ts
+++ src/lib/pocketbase/index.ts
@@ -2,11 +2,4 @@ import PocketBase from 'pocketbase';
 
 const pb = new PocketBase(process.env.NEXT_PUBLIC_PB_URL);
 
-const authData = await pb
-  .collection('users')
-  .authWithPassword(
-    process.env.NEXT_PUBLIC_PB_USERNAME!,
-    process.env.NEXT_PUBLIC_PB_PASSWORD!,
-);
-
 export default pb;
